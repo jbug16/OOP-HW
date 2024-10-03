a55353575b2fa66cac2008af4b3927c9a1f63081
Jenna Curls <98861047+jbug16@users.noreply.github.com>
2024-10-03 16:39:51 -0400

    Progress

--- Assignment3/Jenna_Curls_Prob6_Assignment3.cpp
+++ Assignment3/Jenna_Curls_Prob6_Assignment3.cpp
@@ -6,6 +6,7 @@ class Spaceship
     private:
         int x;
         int y;
+        string direction;
         string position;
 
     public:
@@ -14,13 +15,99 @@ class Spaceship
         {
             x = 0;
             y = 0;
-            position = "{x: 0, y:0, direction: 'up'}";
+            direction = "up";
+            position = "{x: " + to_string(x) + ", y: " + to_string(y) + ", direction: " + direction + "}";
+        }
+        
+        Spaceship(const string& path)
+        {
+            x = 0;
+            y = 0;
+            direction = "up";
+            
+            int _dir = 0; // 0 = up, 1 = right, 2 = down, 3 = left
+            
+            for (char c : path)
+            {
+                c = toupper(c);
+                if (c == 'L') 
+                {
+                    // Rotate left
+                    _dir = (_dir + 3) % 4;
+                }
+                else if (c == 'R') {
+                    // Rotate right
+                    _dir = (_dir + 1) % 4;
+                }
+            }
+            
+            switch (_dir)
+            {
+                case 0:
+                    direction = "up";
+                    break;
+                case 1:
+                    direction = "right";
+                    break;
+                case 2:
+                    direction = "down";
+                    break;
+                case 3:
+                    direction = "left";
+                    break;
+            }
+            
+            for (char c : path)
+            {
+                // Advance
+                if (c == 'A')
+                {
+                    switch (_dir)
+                    {
+                        case 0:
+                            y++;
+                            break;
+                        case 1:
+                            x++;
+                            break;
+                        case 2:
+                            y--;
+                            break;
+                        case 3:
+                            x--;
+                            break;
+                    }
+                }
+            }
+            
+            position = "{x: " + to_string(x) + ", y: " + to_string(y) + ", direction: " + direction + "}";
+        }
+        
+        int getX()
+        {
+            return x;
+        }
+        
+        int getY()
+        {
+            return y;
+        }
+        
+        string getDirection()
+        {
+            return direction;
+        }
+        
+        string getPosition()
+        {
+            return position;
         }
 };
 
 int main()
 {
+    Spaceship spaceship("LA");
+    cout << spaceship.getPosition();
     
-
     return 0;
 }
