651e85b68fa5571670944bc71fe1f237c5dcc063
Jenna Curls <98861047+jbug16@users.noreply.github.com>
2024-11-10 17:14:11 -0500

    A4 Complete

--- Assignment4/Jenna_Curls_Prob2_Assignment4.cpp
+++ Assignment4/Jenna_Curls_Prob2_Assignment4.cpp
@@ -1,22 +1,87 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-class Person
+class Person 
 {
     private:
-        char *name; // Pointer to the name
-		int age;
+        int age;
+        char *name;
 
     public:
-        // Constructor
-        Person(const char *n, int a)
-		{
-			age = a;
-			name = new char;
-		}
+        Person(const char *nameInput, int ageInput)
+        {
+            // Allocate memory for the name
+            name = new char[strlen(nameInput) + 1];
+            // Copy name
+            strcpy(name, nameInput);
+
+            age = ageInput;
+        }
+
+        // Deep copy
+        Person(const Person &p)
+        {
+            // Allocate memory for name
+            name = new char[strlen(p.name) + 1];
+            // Copy naame
+            strcpy(name, p.name);
+
+            age = p.age;
+        }
+
+        // Deconstuctor
+        ~Person()
+        {
+            // Free memory for name
+            delete[] name;
+        }
+
+        void display() const
+        {
+            cout << "Name: " << name << endl;
+            cout << "Age: " << age << endl;
+        }
+
+        void setName(const char *newName)
+        {
+            // Free mmemory
+            delete[] name;
+            // Allocate new memory
+            name = new char[strlen(newName) + 1];
+            // Copy new name
+            strcpy(name, newName);
+        }
+
+        void setAge(int newAge)
+        {
+            age = newAge;
+        }
 };
 
-int main()
+int main() 
 {
+    // Create Caleb
+    cout << endl << "--- Create Person Caleb ---" << endl << endl;
+    Person person1("Caleb", 30);
+    person1.display();
+
+    // Create person2
+    cout << endl << "--- Person2 = Person1 ---" << endl << endl;
+    Person person2 = person1;
+    person2.display();
+
+    // Change person2 name to Jenna
+    cout << endl << "--- Change Caleb to Jenna (Shallow Copy) ---" << endl << endl;
+    person2.setName("Jenna");
+    person2.display();
+    person1.display(); // Check if person1 is changed too, because a shallow copy should change person1
+
+    // Deep copy example
+    cout << endl << "--- Deep Copy Example ---" << endl << endl;
+    Person person3(person1);
+    person3.display();
+    person1.display();
+    
     return 0;
 }
--- Assignment4/Jenna_Curls_Prob5_Assignment4.cpp
+++ Assignment4/Jenna_Curls_Prob5_Assignment4.cpp
@@ -2,58 +2,37 @@
 #include <ctime>
 using namespace std;
 
-class MyDate
-{
+class MyDate {
     private:
         int year;
         int month;
         int day;
 
     public:
-        MyDate()
+        MyDate() 
         {
-            time_t now = time(0);
-
-            tm* localtm = localtime(&now);
-
-            year = localtm->tm_year + 1900;
-            month = localtm->tm_mon;
-            day = localtm->tm_mday;
+            time_t t = time(0);
+            struct tm* now = localtime(&t);
+            year = now->tm_year + 1900;
+            month = now->tm_mon;
+            day = now->tm_mday;
         }
 
-        MyDate(long elapsedTime)
+        MyDate(long elapsedTime) 
         {
-            time_t rawTime = elapsedTime;
-            tm* timeInfo = gmtime(&rawTime);
-            year = timeInfo->tm_year + 1900;
-            month = timeInfo->tm_mon;
-            day = timeInfo->tm_mday;
+            struct tm* dt = gmtime(&elapsedTime);
+            year = dt->tm_year + 1900;
+            month = dt->tm_mon;
+            day = dt->tm_mday;
         }
 
-        MyDate(int y, int m, int d)
+        MyDate(int year, int month, int day)
         {
-            year = y;
-            month = m;
-            day = d;
+            year = year;
+            month = month;
+            day = day;
         }
 
-        // Setters
-        void setYear(int y) 
-        {
-            year = y;
-        }
-
-        void setMonth(int m) 
-        {
-            month = m;
-        }
-
-        void setDay(int d) 
-        {
-            day = d;
-        }
-
-        // Getters
         int getYear() const 
         {
             return year;
@@ -69,9 +48,38 @@ class MyDate
             return day;
         }
 
+        void setYear(int year) 
+        {
+            this->year = year;
+        }
+
+        void setMonth(int month) 
+        {
+            this->month = month;
+        }
+
+        void setDay(int day) 
+        {
+            this->day = day;
+        }
+
+        void setDate(long elapsedTime) 
+        {
+            struct tm* dt = gmtime(&elapsedTime);
+            year = dt->tm_year + 1900;
+            month = dt->tm_mon;
+            day = dt->tm_mday;
+        }
 };
 
-int main()
-{
+int main() {
+
+    MyDate date1;
+    MyDate date2(3435555513);
+
+    cout << "Current Date: " << date1.getYear() << "/" << date1.getMonth() + 1 << "/" << date1.getDay() << endl;
+    cout << "Custom Date: " << date2.getYear() << "/" << date2.getMonth() + 1 << "/" << date2.getDay() << endl;
+
     return 0;
+
 }
